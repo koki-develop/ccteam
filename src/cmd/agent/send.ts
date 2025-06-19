09384e0d3f3cbd0f15255910411717bf765bcf23
koki-develop <kou.pg.0131@gmail.com>
2025-06-19 20:56:40 +0900

    chore: Add sent log

--- src/cmd/agent/send.ts
+++ src/cmd/agent/send.ts
@@ -4,4 +4,5 @@ import { loadSessionName } from "../../lib/util";
 export async function sendCommand(role: string, message: string) {
   const session = await loadSessionName();
   await send({ session, role, message });
+  console.log(`[INFO] Message sent to ${role}`);
 }
